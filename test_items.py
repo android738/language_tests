14d62f40df49e86e7fb7bc797b90c3c0a145ebca
Vladislav Kalinin <android7396@gmail.com>
2022-04-14 10:47:47 +0400

    Added browser language tests

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_browser_languages(browser):
+    browser.get(link)
+    wait = WebDriverWait(browser, 15)
+    # Дожидаемся полной загрузки страницы, опираясь на элемент описания продукта
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#product_description"))) 
+    # time.sleep(30)
+    # Проверяем наличие кнопки добавления в корзину (что количество элементов, найденных по данному селектору больше нуля)
+    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) > 0, "\"Add to basket\" button does not exists"
