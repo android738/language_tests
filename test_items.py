bdbd07e4ff220640026dafc11002f858a9c020d5
Vladislav Kalinin <android7396@gmail.com>
2022-04-14 12:09:01 +0400

    Added try/catch

--- test_items.py
+++ test_items.py
@@ -8,8 +8,11 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_browser_languages(browser):
     browser.get(link)
     wait = WebDriverWait(browser, 15)
-    # Дожидаемся полной загрузки страницы, опираясь на элемент описания продукта
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#product_description"))) 
-    # time.sleep(30)
-    # Проверяем наличие кнопки добавления в корзину (что количество элементов, найденных по данному селектору больше нуля)
-    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) > 0, "\"Add to basket\" button does not exists"
+    try:
+        # Дожидаемся полной загрузки страницы, опираясь на элемент описания продукта
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#product_description"))) 
+        time.sleep(30)
+        # Проверяем наличие кнопки добавления в корзину (что количество элементов, найденных по данному селектору больше нуля)
+        assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) > 0, "\"Add to basket\" button does not exists"
+    except Exception:
+        raise ValueError("Не удалось загрузить страницу.")
